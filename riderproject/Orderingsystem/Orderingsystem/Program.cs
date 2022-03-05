bcf398bbabae129b6d7b31645dee9fc4d2e6f5f1
e-hennep <e-hennep@asvg.no>
2022-03-05 19:53:56 +0100

    uncomment

--- riderproject/Orderingsystem/Orderingsystem/Program.cs
+++ riderproject/Orderingsystem/Orderingsystem/Program.cs
@@ -43,13 +43,13 @@ builder.Services.AddScoped<IOrderService, OrderService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<IUserService, UserService>();
 
-/*builder.WebHost.UseKestrel(serverOptions =>
+builder.WebHost.UseKestrel(serverOptions =>
 {
     serverOptions.Listen(IPAddress.Any, 5000, listenOptions =>
     {
         listenOptions.UseHttps(new X509Certificate2("certificate.pfx", "Passord01"));
     });
-});*/
+});
 
 builder.Services.AddCors(o => o.AddPolicy("CorsPolicy", b =>
 {
